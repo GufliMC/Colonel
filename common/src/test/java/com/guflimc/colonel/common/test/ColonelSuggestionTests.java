72e6f6a1514055714d8c15d9f8bfe858f57704b9
Joris Guffens <joris.guffens@outlook.com>
2023-04-24 15:47:40 +0200

    unit tests for argument suggestions but no working implementation yet

--- common/src/test/java/com/guflimc/colonel/common/test/ColonelSuggestionTests.java
+++ common/src/test/java/com/guflimc/colonel/common/test/ColonelSuggestionTests.java
@@ -1,6 +1,7 @@
 package com.guflimc.colonel.common.test;
 
 import com.guflimc.colonel.common.Colonel;
+import com.guflimc.colonel.common.build.Argument;
 import com.guflimc.colonel.common.suggestion.Suggestion;
 import com.guflimc.colonel.common.test.util.Person;
 import org.junit.jupiter.api.Test;
@@ -103,6 +104,95 @@ public class ColonelSuggestionTests {
         assertEquals(List.of(new Suggestion("baz")), suggestions);
 
         suggestions = colonel.suggestions(person, "foo bar z");
+        assertEquals(List.of(), suggestions);
+    }
+
+    //
+
+    @Test
+    public void suggestFirstArgument() {
+        colonel.register("foo bar", b -> b
+                .word("p1", (ctx, val) -> Argument.success(null), (ctx, input) -> List.of(new Suggestion("fizz"), new Suggestion("buzz")))
+                .word("p2", (ctx, val) -> Argument.success(null), (ctx, input) -> List.of(new Suggestion("hello"), new Suggestion("world")))
+                .word("p3", (ctx, val) -> Argument.success(null), (ctx, input) -> List.of(new Suggestion("hi"), new Suggestion("mom")))
+                .executor(ctx -> {})
+        );
+
+        List<Suggestion> suggestions = colonel.suggestions(person, "foo bar ");
+        assertEquals(List.of(new Suggestion("fizz"), new Suggestion("buzz")), suggestions);
+    }
+
+    @Test
+    public void suggestFirstArgumentWithPrefix() {
+        colonel.register("foo bar", b -> b
+                .word("p1", (ctx, val) -> Argument.success(null), (ctx, input) -> List.of(new Suggestion("fizz"), new Suggestion("buzz")))
+                .word("p2", (ctx, val) -> Argument.success(null), (ctx, input) -> List.of(new Suggestion("hello"), new Suggestion("world")))
+                .word("p3", (ctx, val) -> Argument.success(null), (ctx, input) -> List.of(new Suggestion("hi"), new Suggestion("mom")))
+                .executor(ctx -> {})
+        );
+
+        List<Suggestion> suggestions = colonel.suggestions(person, "foo bar buz");
+        assertEquals(List.of(new Suggestion("buzz")), suggestions);
+
+        suggestions = colonel.suggestions(person, "foo bar zap");
+        assertEquals(List.of(), suggestions);
+    }
+
+    @Test
+    public void suggestMiddleArgument() {
+        colonel.register("foo bar", b -> b
+                .word("p1", (ctx, val) -> Argument.success(null), (ctx, input) -> List.of(new Suggestion("fizz"), new Suggestion("buzz")))
+                .word("p2", (ctx, val) -> Argument.success(null), (ctx, input) -> List.of(new Suggestion("hello"), new Suggestion("world")))
+                .word("p3", (ctx, val) -> Argument.success(null), (ctx, input) -> List.of(new Suggestion("hi"), new Suggestion("mom")))
+                .executor(ctx -> {})
+        );
+
+        List<Suggestion> suggestions = colonel.suggestions(person, "foo bar fizz ");
+        assertEquals(List.of(new Suggestion("hello"), new Suggestion("world")), suggestions);
+    }
+
+    @Test
+    public void suggestMiddleArgumentWithPrefix() {
+        colonel.register("foo bar", b -> b
+                .word("p1", (ctx, val) -> Argument.success(null), (ctx, input) -> List.of(new Suggestion("fizz"), new Suggestion("buzz")))
+                .word("p2", (ctx, val) -> Argument.success(null), (ctx, input) -> List.of(new Suggestion("hello"), new Suggestion("world")))
+                .word("p3", (ctx, val) -> Argument.success(null), (ctx, input) -> List.of(new Suggestion("hi"), new Suggestion("mom")))
+                .executor(ctx -> {})
+        );
+
+        List<Suggestion> suggestions = colonel.suggestions(person, "foo bar fizz h");
+        assertEquals(List.of(new Suggestion("hello")), suggestions);
+
+        suggestions = colonel.suggestions(person, "foo bar fizz zap");
+        assertEquals(List.of(), suggestions);
+    }
+
+    @Test
+    public void suggestLastArgument() {
+        colonel.register("foo bar", b -> b
+                .word("p1", (ctx, val) -> Argument.success(null), (ctx, input) -> List.of(new Suggestion("fizz"), new Suggestion("buzz")))
+                .word("p2", (ctx, val) -> Argument.success(null), (ctx, input) -> List.of(new Suggestion("hello"), new Suggestion("world")))
+                .word("p3", (ctx, val) -> Argument.success(null), (ctx, input) -> List.of(new Suggestion("hi"), new Suggestion("mom")))
+                .executor(ctx -> {})
+        );
+
+        List<Suggestion> suggestions = colonel.suggestions(person, "foo bar fizz hello ");
+        assertEquals(List.of(new Suggestion("hi"), new Suggestion("mom")), suggestions);
+    }
+
+    @Test
+    public void suggestLastArgumentWithPrefix() {
+        colonel.register("foo bar", b -> b
+                .word("p1", (ctx, val) -> Argument.success(null), (ctx, input) -> List.of(new Suggestion("fizz"), new Suggestion("buzz")))
+                .word("p2", (ctx, val) -> Argument.success(null), (ctx, input) -> List.of(new Suggestion("hello"), new Suggestion("world")))
+                .word("p3", (ctx, val) -> Argument.success(null), (ctx, input) -> List.of(new Suggestion("hi"), new Suggestion("mom")))
+                .executor(ctx -> {})
+        );
+
+        List<Suggestion> suggestions = colonel.suggestions(person, "foo bar fizz hello mom");
+        assertEquals(List.of(new Suggestion("mom")), suggestions);
+
+        suggestions = colonel.suggestions(person, "foo bar fizz hello zap");
         assertEquals(List.of(), suggestions);
     }
 
